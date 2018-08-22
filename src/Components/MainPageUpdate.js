29b47ff05591381c496a0cfb27134be19c465fe7
Audiovisuaali <aleksi.vuorio@yahoo.com>
2018-08-22 15:09:00 +0300

    Update MainPageUpdate.js

--- src/Components/MainPageUpdate.js
+++ src/Components/MainPageUpdate.js
@@ -1,42 +1,39 @@
 import React, { Component } from 'react';
 
-export default class MainPageUpdate extends Component {
-    render() {
-        const asd = new Date(this.props.update.timeStamp)
-        const das = asd.getFullYear() + "." + ("0" + asd.getMonth()).slice(-2) + "." + ("0" + asd.getDate()).slice(-2) + " " + ("0" + asd.getHours()).slice(-2) + ":" + ("0" + asd.getMinutes()).slice(-2);
-        return (
-            <div className="col s12 m6">
-            <div className="update">
-                <h5>{ this.props.update.title } <span>({das})</span></h5>
-                <div className="content">
-                    {
-                        this.props.update.changes.map(function(change) {
-                            return( <MainPageUpdateChange update={change} /> )
-                        })
-                    }
-                </div>
+const MainPageUpdate = props => {
+    const t = new Date(props.update.timeStamp)
+    const das = t.getFullYear() + "." + ("0" + t.getMonth()).slice(-2) + "." + ("0" + t.getDate()).slice(-2) + " " + ("0" + t.getHours()).slice(-2) + ":" + ("0" + t.getMinutes()).slice(-2);
+    return (
+        <div className="col s12 m6">
+        <div className="update">
+            <h5>{ props.update.title } <span>({das})</span></h5>
+            <div className="content">
+                {
+                    props.update.changes.map(function(change) {
+                        return( <MainPageUpdateChange update={change} /> )
+                    })
+                }
             </div>
-            </div>
-        )
-    }
+        </div>
+        </div>
+    )
 }
+export default MainPageUpdate;
 
-class MainPageUpdateChange extends Component {
-    render() {
-        if ( this.props.update.type === "+") {
+const MainPageUpdateChange = props => {
+    if ( props.update.type === "+") {
             return(
                 <p><i className="fas fa-plus"></i> {this.props.update.message}</p>
             )
         }
-        if ( this.props.update.type === "-") {
+        if ( props.update.type === "-") {
             return(
                 <p><i className="fas fa-minus"></i> {this.props.update.message}</p>
             )
         }
-        if ( this.props.update.type === "/") {
+        if ( props.update.type === "/") {
             return(
                 <p><i className="fas fa-wrench"></i> {this.props.update.message}</p>
             )
         }
-    }
 }
