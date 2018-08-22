bedfeecc9e46f0dbcb7063688743e35e96c8bc73
Audiovisuaali <aleksi.vuorio@yahoo.com>
2018-08-22 15:05:32 +0300

    Update MainPageLoading.js

--- src/Components/MainPageLoading.js
+++ src/Components/MainPageLoading.js
@@ -4,19 +4,18 @@ import ReactCSSTransitionGroup from "react-addons-css-transition-group";
 import Loading from "../Assets/DoubleRingLoading.svg";
 import "../Assets/Transitions.css";
 
-export default class MainPageContent extends Component {
-    render() {
-        return (
-            <ReactCSSTransitionGroup 
-                component="div"
-                className="loader loadshadow"
-                transitionName="route"
-                transitionEnterTimeout={60}
-                transitionLeaveTimeout={40}
-                transitionAppearTimeout={60}
-                transitionAppear={true}>
-                <img src={Loading} alt="WhiteDust Loader"/>
-            </ReactCSSTransitionGroup>
-        )
-    }
+const MainPageContent = () => {
+    return (
+        <ReactCSSTransitionGroup 
+            component="div"
+            className="loader loadshadow"
+            transitionName="route"
+            transitionEnterTimeout={60}
+            transitionLeaveTimeout={40}
+            transitionAppearTimeout={60}
+            transitionAppear={true}>
+            <img src={Loading} alt="WhiteDust Loader"/>
+        </ReactCSSTransitionGroup>
+    )
 }
+export default MainPageContent;
