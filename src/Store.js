f7e1b027f0f780d0f068da5446dfefa32d8ee5c7
Audiovisuaali <aleksi.vuorio@yahoo.com>
2018-08-22 15:04:39 +0300

    Update Store.js

--- src/Store.js
+++ src/Store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
 
-import logger from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import promise from "redux-promise-middleware";
 
@@ -8,8 +7,7 @@ import Reducers from "./Reducers/Reducers";
 
 const store = createStore(Reducers, applyMiddleware(
   promise(),
-  thunkMiddleware,
-  logger)
+  thunkMiddleware)
 );
 
 export default store;
